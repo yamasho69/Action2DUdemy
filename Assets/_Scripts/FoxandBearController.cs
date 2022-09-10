a8c48e2e478d599bc2545290d4ea76c9bfd62fc2
yamasho69 <nitiham55@yahoo.co.jp>
2022-09-10 21:05:43 +0900

    8回目のコミット
    リス、クマ、狐を実装

---
+++ Assets/_Scripts/FoxandBearController.cs
@@ -0,0 +1,210 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+//using DG.Tweening;
+
+public class FoxandBearController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Animator enemyAnim;
+
+    //同じ型の変数ならカンマで区切って連続で宣言できる
+    [SerializeField]
+    private float moveSpeed, waitTime, walkTime;
+
+    private float waitCounter, moveCounter;
+
+    private Vector2 moveDir;
+
+    [SerializeField]
+    private BoxCollider2D area;
+
+    [SerializeField, Tooltip("プレイヤーを追いかける")]
+    private bool chase;
+
+    private bool isChaseing;
+
+    [SerializeField]
+    private float chaseSpeed, rangeToChase;
+    private Transform target;
+
+    [SerializeField]
+    private float waitAfterHitting;
+
+    /*[SerializeField]
+    private int attaackDamage;
+
+    [SerializeField]
+    private float maxHealth;
+    private float currentHealth;
+
+    private bool isKnockingBack;
+
+    [SerializeField]
+    private float knockBackTime, knockBackForce;
+
+    private float knockBackCounter;
+
+    private Vector2 knockDir;
+
+    [SerializeField]
+    private GameObject portion;
+
+    [SerializeField]
+    private float healthDropChance;
+
+    [SerializeField]
+    private GameObject blood;
+
+    [SerializeField]
+    private int exp;
+
+    [SerializeField]
+    private Image hpImage;
+
+    private Flash flash;*/
+
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+        enemyAnim = GetComponent<Animator>();
+
+        waitCounter = waitTime;
+
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+
+        /*currentHealth = maxHealth;
+        UpdateHealthImage();
+        flash = GetComponent<Flash>();*/
+    }
+
+    void Update() {
+        /*if (isKnockingBack) {
+            if (knockBackCounter > 0) {
+                knockBackCounter -= Time.deltaTime;
+                rb.velocity = knockDir * knockBackForce;
+            } else {
+                rb.velocity = Vector2.zero;
+                isKnockingBack = false;
+            }
+            return;
+        }*/
+
+        if (!isChaseing) {//追いかけてないときは彷徨う
+            if (waitCounter > 0) {
+                waitCounter -= Time.deltaTime;
+                rb.velocity = Vector2.zero;
+                if (waitCounter <= 0) {
+                    moveCounter = walkTime;
+                    enemyAnim.SetBool("moving", true);
+                    moveDir = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+                    moveDir.Normalize();
+                    
+                    if(Mathf.Abs(moveDir.x)>= Mathf.Abs(moveDir.y)) {
+                        if (moveDir.x >= 0) {
+                            enemyAnim.SetFloat("X", 1f);//アニメーターのパラメータXを１に
+                            enemyAnim.SetFloat("Y", 0);
+                        } else {
+                            enemyAnim.SetFloat("X", -1f);//アニメーターのパラメータXを‐１に
+                            enemyAnim.SetFloat("Y", 0);
+                        }
+                    } else {
+                        if (moveDir.y >= 0) {
+                            enemyAnim.SetFloat("X", 0);
+                            enemyAnim.SetFloat("Y", 1);//アニメーターのパラメータYを１に
+                        } else {
+                            enemyAnim.SetFloat("X", 0);
+                            enemyAnim.SetFloat("Y", -1);//アニメーターのパラメータYを-１に
+                        }
+                    }
+                }
+            } else {
+                moveCounter -= Time.deltaTime;
+                rb.velocity = moveDir * moveSpeed;
+                if (moveCounter <= 0) {
+                    enemyAnim.SetBool("moving", false);
+                    waitCounter = waitTime;
+                }
+            }
+
+            if (chase) {
+                if (Vector3.Distance(transform.position, target.transform.position) < rangeToChase) {//敵とプレイヤーの距離がrangeToChaseより小さい時
+                    isChaseing = true;
+                }
+            }
+        } else {
+            if (waitCounter > 0) {
+                waitCounter -= Time.deltaTime;
+                rb.velocity = Vector2.zero;
+
+                if (waitCounter <= 0) {
+                    enemyAnim.SetBool("moving", true);
+                }
+            } else {
+                moveDir = target.transform.position - transform.position;
+                moveDir.Normalize();
+                rb.velocity = moveDir * chaseSpeed;
+            }
+            if (Vector3.Distance(transform.position, target.transform.position) > rangeToChase) {//敵とプレイヤーの距離がrangeToChaseより大きい時
+                isChaseing = false;
+
+                waitCounter = waitTime;
+
+                enemyAnim.SetBool("moving", false);
+            }
+        }
+
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, area.bounds.min.x + 1, area.bounds.max.x - 1)
+            , Mathf.Clamp(transform.position.y, area.bounds.min.y + 1, area.bounds.max.y - 1), transform.position.z);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.tag == "Player") {
+            if (isChaseing) {
+                RisuController player = collision.gameObject.GetComponent<RisuController>();
+                //player.KnockBack(transform.position);
+                player.DamagePlayer();
+
+                waitCounter = waitAfterHitting;
+
+                enemyAnim.SetBool("moving", false);
+            }
+        }
+    }
+
+    /*public void KnockBack(Vector3 position) {
+        isKnockingBack = true;
+        knockBackCounter = knockBackTime;
+        knockDir = transform.position - position;
+        knockDir.Normalize();
+
+        enemyAnim.SetBool("moving", false);
+    }
+
+    public void TakeDamage(int damage, Vector3 position) {
+        currentHealth -= damage;
+        UpdateHealthImage();
+        flash.PlayFeedback();
+        if (currentHealth <= 0) {
+
+            Instantiate(blood, transform.position, transform.rotation);
+
+            GameManager.instance.AddExp(exp);
+
+            if (currentHealth <= 0) {
+                if (Random.Range(0, 100) < healthDropChance && portion != null) {
+                    Instantiate(portion, transform.position, transform.rotation);
+                }
+            }
+
+            Destroy(gameObject);
+        }
+        KnockBack(position);
+    }
+
+    private void UpdateHealthImage() {
+        hpImage.fillAmount = currentHealth / maxHealth;
+    }*/
+}
---
+++ Assets/_Scripts/RisuController.cs
@@ -0,0 +1,196 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+//using DG.Tweening;
+
+public class RisuController : MonoBehaviour {
+    [SerializeField, Tooltip("移動スピード")]
+    private int moveSpeed;
+    [SerializeField]
+    private Animator playerAnim;
+    //[SerializeField]
+    //private Animator weaponAnim;
+    public Rigidbody2D rb;
+
+    //[System.NonSerialized]//他の関数からは参照するがインスペクタには表示したくない場合
+    //public int currentHealth;
+    //public int maxHealth;
+
+    //private bool isknockingback;
+    //private Vector2 knockDir;
+
+    //[SerializeField]
+    //private float knockbackTime, knockbackForce;
+    //private float knockbackCounter;
+
+    //[SerializeField]
+    //private float invincibilityTime;
+    //private float invincibilityCounter;
+
+    public float totalStamina, recoverySpeed;
+    [System.NonSerialized]
+    public float currentStamina;
+
+    [SerializeField]
+    private float dashSpeed, dashLength, dashCost;
+
+    private float dashCounter, activeMoveSpeed;
+
+    //private Flash flash;
+
+    void Start() {
+        //currentHealth = maxHealth;
+
+        //GameManager.instance.UpdateHealthUI();
+
+        activeMoveSpeed = moveSpeed;
+
+        currentStamina = totalStamina;
+
+        GameManager.instance.UpdateStaminaUI();
+
+        //flash = GetComponent<Flash>();
+    }
+
+    void Update() {
+        if (GameManager.instance.statusPanal.activeInHierarchy) {
+            return;
+        }
+
+        //無敵時間中かどうか
+        /*if (invincibilityTime > 0) {
+            invincibilityCounter -= Time.deltaTime;
+        }
+
+        //ノックバック中かどうか
+        if (isknockingback) {
+            knockbackCounter -= Time.deltaTime;
+            rb.velocity = knockDir * knockbackForce;
+
+            if (knockbackCounter <= 0) {
+                isknockingback = false;
+            } else {
+                return;//ノックバック中なら、これ以降は処理しない
+            }
+        }*/
+
+
+        //.normalizedでベクトルの大きさを正規化し、斜め移動のスピードを上下左右の移動スピードに合わせる
+        rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized * activeMoveSpeed;
+
+        if (rb.velocity != Vector2.zero) {//移動スピードがゼロではない時
+            playerAnim.SetBool("moving", true);
+            if (Input.GetAxisRaw("Horizontal") != 0) {//左右の入力がゼロではない時              
+                if (Input.GetAxisRaw("Horizontal") > 0) {//右に移動しているとき
+                    playerAnim.SetFloat("X", 1f);//アニメーターのパラメータXを１に
+                    playerAnim.SetFloat("Y", 0);
+
+                    /*
+                    weaponAnim.SetFloat("X", 1f);//アニメーターのパラメータXを１に
+                    weaponAnim.SetFloat("Y", 0);
+                    */
+
+                } else {
+                    playerAnim.SetFloat("X", -1f);//アニメーターのパラメータXを‐１に
+                    playerAnim.SetFloat("Y", 0);
+
+                    /*
+                    weaponAnim.SetFloat("X", -1f);//アニメーターのパラメータXを‐１に
+                    weaponAnim.SetFloat("Y", 0);
+                    */
+
+                }
+            } else if (Input.GetAxisRaw("Vertical") > 0) {//上方向への入力があるとき
+                playerAnim.SetFloat("X", 0);
+                playerAnim.SetFloat("Y", 1);//アニメーターのパラメータYを１に
+
+                /*
+                weaponAnim.SetFloat("X", 0);
+                weaponAnim.SetFloat("Y", 1);//アニメーターのパラメータYを１に
+                */
+
+            } else {
+                playerAnim.SetFloat("X", 0);
+                playerAnim.SetFloat("Y", -1);//アニメーターのパラメータYを-１に
+
+                /*
+                weaponAnim.SetFloat("X", 0);
+                weaponAnim.SetFloat("Y", -1);//アニメーターのパラメータYを-１に
+                */
+            }
+        } else {
+            playerAnim.SetBool("moving", false);
+        }
+        /*if (Input.GetMouseButtonDown(0)) {
+            weaponAnim.SetTrigger("Attack");
+        }*/
+
+        if (dashCounter <= 0) {
+            //ローマ字入力モードだと上手くダッシュが機能しないことがあった
+            if (Input.GetKeyDown(KeyCode.Space) && currentStamina > dashCost) {
+                activeMoveSpeed = dashSpeed;
+                dashCounter = dashLength;
+
+                currentStamina -= dashCost;
+
+                GameManager.instance.UpdateStaminaUI();
+            }
+        } else {
+            dashCounter -= Time.deltaTime;
+            if (dashCounter <= 0) {
+                activeMoveSpeed = moveSpeed;
+            }
+        }
+
+        currentStamina = Mathf.Clamp(currentStamina + recoverySpeed * Time.deltaTime, 0, totalStamina);
+        GameManager.instance.UpdateStaminaUI();
+    }
+
+    /// <summary>
+    /// 吹き飛ばし用の関数
+    /// </summary>
+    /// <param name="position"></param>
+
+    /*
+    public void KnockBack(Vector3 position) {
+        knockbackCounter = knockbackTime;
+        isknockingback = true;
+
+        knockDir = transform.position - position;
+        knockDir.Normalize();
+    }*/
+
+    public void DamagePlayer() {
+        //ダメージを食らって、HPが0以下にならないようにClampで調整
+        //if (invincibilityCounter <= 0) {
+            //flash.PlayFeedback();
+
+            //currentHealth = Mathf.Clamp(currentHealth - damage, 0, maxHealth);
+
+            //invincibilityCounter = invincibilityTime;
+
+            SoundManager.instance.PlaySE(2);
+
+            //if (currentHealth == 0) {
+                gameObject.SetActive(false);
+                SoundManager.instance.PlaySE(0);
+                GameManager.instance.Load();
+            //}
+        //}
+        //GameManager.instance.UpdateHealthUI();
+    }
+
+    /*private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.tag == "portion" && maxHealth != currentHealth && collision.GetComponent<Items>().waitTime <= 0) {
+            Items items = collision.GetComponent<Items>();
+            SoundManager.instance.PlaySE(1);
+            currentHealth = Mathf.Clamp(currentHealth + items.helthItemRecoveryValue, 0, maxHealth);
+            GameManager.instance.UpdateHealthUI();
+            Destroy(collision.gameObject);
+        }
+    }*/
+}
